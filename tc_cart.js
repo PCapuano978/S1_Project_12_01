72d45214a272ff05b5a05cafcd0c45154ca6250c
Unknown <pcapuano978@west-mec.org>
2019-02-27 15:25:14 -0700

    Project Started; Variables Declared
    I stared off this project build by establishing the variables in "tc_order.js" to be used for calculations in "tc_cart.js". I also started a large "for" loop that will be used for those calculation.

---
+++ tc_cart.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: Patrick M. Capuano
+   Date:   2.27.19
+   
+   Filename: tc_cart.js
+	
+*/
+
+// Declaring the varaible "orderTotal" for later use in cost calculation and is currently set to an initial value of zero.
+var orderTotal = 0;
+
+// Variable that will contain the HTML code for the contents of the shopping cart, which is displayed as a table.
+var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> </tr>"
+
+// 
+if (cartHTML = 0; i < ) cartHTML += "<tr> <td><img src='tc_" + item + ".png' alt='" + item + "' /></td>";
